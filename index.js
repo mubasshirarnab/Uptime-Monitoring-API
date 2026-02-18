4d6b8af47d69c02fbf6453d0a132d7d1735e64bf
unknown <arnab0574@gmail.com>
2026-02-19 00:43:21 +0600

    Checking the file system

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 const http = require('http')
 const {hadleReqRes} = require('./helpers/handleReqRes')
 const environments = require('./helpers/environments')
-
+const data = require('./lib/data')
 
 //app object - module scaffolding
 const app = {}
 
+//testing file system
+data.create('test', 'newFile', {name : 'Bangladesh', Language : 'Bangla'}, (err) =>{
+    console.log(`Error was`, err)
+})
 
 //create a server
 app.createServer = () => {
