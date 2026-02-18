81addd2ee1b117e2ffb0ba1614ac94848823c471
unknown <arnab0574@gmail.com>
2026-02-18 17:32:56 +0600

    parsing the url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Dependencies
 const http = require('http')
+const url = require('url')
 
 //app object - module scaffolding
 const app = {}
@@ -20,6 +21,22 @@ app.createServer = () => {
 
 //handle Request and Response
 app.handleReqRes = (req, res) => {
+    //handling the Request
+
+    //Getting the URL and Parse it
+    const parsedUrl = url.parse(req.url, true)
+    const path = parsedUrl.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '') 
+    const method = req.method.toLowerCase()
+    const queryStringObject = parsedUrl.query
+    const header = req.headers
+
+
+
+    console.log(method)
+
+
+    //Handling the response
     res.end("Hello World!!!!")
 }
 
