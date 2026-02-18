821cbdf8ed84ba991b425246800818a5ff0ee357
unknown <arnab0574@gmail.com>
2026-02-18 18:36:44 +0600

    Refactor the codebase. Create seperate folder for Req Res handler

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //Dependencies
 const http = require('http')
-const url = require('url')
-const {StringDecoder} = require('string_decoder')
-const { buffer } = require('stream/consumers')
+const {hadleReqRes} = require('./helpers/handleReqRes')
+
 
 //app object - module scaffolding
 const app = {}
@@ -22,35 +21,7 @@ app.createServer = () => {
 }
 
 //handle Request and Response
-app.handleReqRes = (req, res) => {
-    //handling the Request
-
-    //Getting the URL and Parse it
-    const parsedUrl = url.parse(req.url, true)
-    const path = parsedUrl.pathname
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '') 
-    const method = req.method.toLowerCase()
-    const queryStringObject = parsedUrl.query
-    const header = req.headers
-
-
-    const decoder = new StringDecoder('utf-8')
-    let realData = ''
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer)
-    })
-
-    req.on('end', () =>{
-        realData += decoder.end()
-        console.log(realData)
-
-        //Handling the response
-        res.end("Hello World!!!!")
-    })
-
-    
-}
+app.handleReqRes = hadleReqRes
 
 //start the server
 app.createServer()
