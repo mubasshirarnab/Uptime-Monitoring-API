a558ed2cb27de0780bbb5258173a441d6b4d0600
unknown <arnab0574@gmail.com>
2026-02-20 01:50:45 +0600

    Put method

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -100,7 +100,63 @@ handler._user.get = (requestProperties, callback) => {
 }
 
 handler._user.put = (requestProperties, callback) => {
+    //Sanitizing the user info
+    const firstName = typeof(requestProperties.body.firstName) === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false
 
+    const lastName = typeof(requestProperties.body.lastName) === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false
+
+    const phone = typeof(requestProperties.queryStringObject.phone) === 'string' && requestProperties.queryStringObject.phone.trim().length == 11 ? requestProperties.queryStringObject.phone : false
+
+    const password = typeof(requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false
+
+    //required phone for updating a user
+    if(phone){
+        if(firstName || lastName || password){
+            data.read('users', phone, (err, user) => {
+                if(!err && user){
+                    const userObject = parseJSON(user)
+                    if(firstName){
+                        userObject.firstName = firstName
+                    }
+                    if(lastName){
+                        userObject.lastName = lastName
+                    }
+                    if(password){
+                        userObject.password = hash(password)
+                    }
+
+                    //Store the updated user to the DB
+                    data.update('users', phone, userObject, (err2) => {
+                        if(!err2){
+                            callback(200, {
+                                message : 'The user updated successfully...'
+                            })
+                        }
+                        else{
+                            callback(500, {
+                                error : 'Could not update user...'
+                            })
+                        }
+                    })
+                }
+                else{
+                    callback(400, {
+                        error : 'The user is not exists...'
+                    })
+                }
+            })
+        }
+        else{
+            callback(400, {
+                error : 'You have a problem in your request...'
+            })
+        }
+    }
+    else{
+        callback(400, {
+            error : 'Invalid phone number. Please try again...'
+        })
+    }
 }
 
 handler._user.delete = (requestProperties, callback) => {
