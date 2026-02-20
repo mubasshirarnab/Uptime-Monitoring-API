503a528ed2399c8a7e93bc5633d24458bdaf6a76
unknown <arnab0574@gmail.com>
2026-02-21 01:50:25 +0600

    Fix the problem in Check Post method

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -33,7 +33,7 @@ handler._check.post = (requestProperties, callback) => {
 
     const url = typeof(requestProperties.body.url) === 'string' && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false
 
-    const method = typeof(requestProperties.body.method) === 'string' && ['get', 'post', 'put', 'delete'].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false
+    const method = typeof(requestProperties.body.method) === 'string' && ['GET', 'POST', 'PUT', 'DELETE'].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false
 
     const successCodes = typeof(requestProperties.body.successCodes) === 'object' && requestProperties.body.successCodes instanceof Array && requestProperties.body.successCodes.length > 0 ? requestProperties.body.successCodes : false
 
@@ -44,78 +44,82 @@ handler._check.post = (requestProperties, callback) => {
         //Get the token from the headers
         const tokenId = typeof(requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token : false  
 
-        //Lookup the user by reading the token        
-        if(!err && tokenData){
-            const tokenObject = parseJSON(tokenData)
-            const userPhone = tokenObject.phone 
-            //Verify the token
-            tokenHandler._token.verify(tokenId, userPhone, (tokenIsValid) => {
-                if(tokenIsValid){
-                    //Create a random id for the check
-                    const checkId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) //The check id should be 22 characters long. Math.random().toString(36).substring(2, 15) generates a random string of 13 characters. So we need to generate two random strings and concatenate them to get a check id of 22 characters long   
+        //Lookup the user by reading the token    
+        data.read('tokens', tokenId, (err, tokenData) => {     
+            //Verify the token and the user is the owner of the token
+            if(!err && tokenData){
+                const tokenObject = parseJSON(tokenData)
+                const userPhone = tokenObject.phone 
+                //Verify the token
+                tokenHandler._token.verify(tokenId, userPhone, (tokenIsValid) => {
+                    if(tokenIsValid){
+                        //Create a random id for the check
+                        const checkId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) //The check id should be 22 characters long. Math.random().toString(36).substring(2, 15) generates a random string of 13 characters. So we need to generate two random strings and concatenate them to get a check id of 22 characters long   
 
-                    //Create the check object and include the user's phone
-                    const checkObject = {
-                        id : checkId,
-                        userPhone,
-                        protocol,
-                        url,
-                        method,
-                        successCodes,
-                        timeoutSeconds
-                    }       
-                    //Save the object
-                    data.create('checks', checkId, checkObject, (err2) => {
-                        if(!err2){
-                            //Add the check id to the user's object
-                            data.read('users', userPhone, (err3, userData) => {
-                                if(!err3 && userData){
-                                    const userObject = parseJSON(userData)
+                        //Create the check object and include the user's phone
+                        const checkObject = {
+                            id : checkId,
+                            userPhone,
+                            protocol,
+                            url,
+                            method,
+                            successCodes,
+                            timeoutSeconds
+                        }       
 
-                                    //Check if the user already has checks
-                                    userObject.checks = typeof(userObject.checks) === 'object' && userObject.checks instanceof Array ? userObject.checks : []
+                        //Save the object
+                        data.create('checks', checkId, checkObject, (err2) => {
+                            if(!err2){
+                                //Add the check id to the user's object
+                                data.read('users', userPhone, (err3, userData) => {
+                                    if(!err3 && userData){
+                                        const userObject = parseJSON(userData)
 
-                                    //Add the check id to the user's object
-                                    userObject.checks.push(checkId)
-                                    //Save the new user data
-                                    data.update('users', userPhone, userObject, (err4) => {
-                                        if(!err4){
-                                            //Return the data about the new check
-                                            callback(200, checkObject)
-                                        }
-                                        else{
-                                            callback(500, {
-                                                error : 'Could not update the user with the new check...'
-                                            })
-                                        }   
-                                    })
-                                }
-                                else{
-                                    callback(500, {
-                                        error : 'Could not find the user who created the check...'
-                                    })
-                                }
-                            })
-                        }
-                        else{
-                            callback(500, {
-                                error : 'Could not create the new check...'
-                            })
-                        }
-                    })
-                }
-                else{
-                    callback(403, {
-                        error : 'Authentication failed...'
-                    })
-                }
-            })
-        }
-        else{
-            callback(403, {
-                error : 'Authentication failed...'
-            })
-        }
+                                        //Check if the user already has checks
+                                        userObject.checks = typeof(userObject.checks) === 'object' && userObject.checks instanceof Array ? userObject.checks : []
+
+                                        //Add the check id to the user's object
+                                        userObject.checks.push(checkId)
+                                        //Save the new user data
+                                        data.update('users', userPhone, userObject, (err4) => {
+                                            if(!err4){
+                                                //Return the data about the new check
+                                                callback(200, checkObject)
+                                            }
+                                            else{
+                                                callback(500, {
+                                                    error : 'Could not update the user with the new check...'
+                                                })
+                                            }   
+                                        })
+                                    }
+                                    else{
+                                        callback(500, {
+                                            error : 'Could not find the user who created the check...'
+                                        })
+                                    }
+                                })
+                            }
+                            else{
+                                callback(500, {
+                                    error : 'Could not create the new check...'
+                                })
+                            }
+                        })
+                    }
+                    else{
+                        callback(403, {
+                            error : 'Authentication failed...'
+                        })
+                    }
+                })
+            }
+            else{
+                callback(403, {
+                    error : 'Authentication failed...'
+                })
+            }
+        })
     }
     else{
         callback(400, {
