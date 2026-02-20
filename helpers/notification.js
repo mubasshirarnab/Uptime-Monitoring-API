3bff4a2e5e1571e5fd7f33dff2cb29d56025d246
unknown <arnab0574@gmail.com>
2026-02-21 03:29:17 +0600

    Create the sructure for send Notification file

---
+++ helpers/notification.js
@@ -0,0 +1,13 @@
+//Dependencies
+const https = require('https')
+
+//Model scarffolding
+const notifications = {}
+
+//Send SMS via Twilio
+notifications.sendTwilioSMS = (phone, msg, callback) => {
+    
+}
+
+//Export the module
+module.exports = notifications
