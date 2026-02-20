386286352bab96e0ea5dc7220644373ad568f24e
unknown <arnab0574@gmail.com>
2026-02-21 02:21:52 +0600

    Create a check - Delete

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -264,7 +264,93 @@ handler._check.put = (requestProperties, callback) => {
 }
 
 handler._check.delete = (requestProperties, callback) => {
-    
+    //Sanitizing the user info
+    const id = typeof(requestProperties.queryStringObject.id) === 'string' && requestProperties.queryStringObject.id.trim().length == 22 ? requestProperties.queryStringObject.id : false
+
+    //Required id for deleting a check
+    if(id){
+        //Look up the check
+        data.read('checks', id, (err, checkData) => {
+            if(!err && checkData){
+                //Parse the check data
+                const checkObject = parseJSON(checkData)
+
+                //Get the token from the headers
+                const tokenId = typeof(requestProperties.headersObject.token) === 'string' ? requestProperties.headersObject.token : false
+
+                //Verify the token and the user is the owner of the token
+                tokenHandler._token.verify(tokenId, checkObject.userPhone, (tokenIsValid) => {
+                    if(tokenIsValid){
+                        //Delete the check data
+                        data.delete('checks', id, (err2) => {
+                            if(!err2){
+                                //Look up the user
+                                data.read('users', checkObject.userPhone, (err3, userData) => {
+                                    if(!err3 && userData){
+                                        //Parse the user data
+                                        const userObject = parseJSON(userData)
+
+                                        //Get the user's checks
+                                        const userChecks = typeof(userObject.checks) === 'object' && userObject.checks instanceof Array ? userObject.checks : []
+
+                                        //Remove the deleted check from the user's list of checks
+                                        const checkPosition = userChecks.indexOf(id)
+
+                                        
+                                        //Find the position of the deleted check on the user's list of checks
+                                        if(checkPosition > -1){
+                                            
+                                            //Splice the check from the user's checks array
+                                            userChecks.splice(checkPosition, 1)
+
+                                            //Re-save the user's data
+                                            data.update('users', checkObject.userPhone, userObject, (err4) => {
+                                                if(!err4){
+                                                    callback(200, {
+                                                        message : 'Check deleted successfully...'
+                                                    })
+                                                }
+                                                else{
+                                                    callback(500, {
+                                                        error : 'Could not update the user...'
+                                                    })
+                                                }
+                                            })
+                                        }
+                                        else{
+                                            callback(500, {
+                                                error : 'Could not find the check on the user\'s object...'
+                                            })
+                                        }
+                                    }
+                                    else{
+                                        callback(500, {
+                                            error : 'Could not find the user who created the check...'
+                                        })
+                                    }
+                                })
+                            }
+                            else{
+                                callback(500, {
+                                    error : 'Could not delete the check...'
+                                })
+                            }
+                        })
+                    }
+                    else{
+                        callback(403, {
+                            error : 'Authentication failed...'
+                        })
+                    }
+                })
+            }
+            else{
+                callback(400, {
+                    error : 'Check not found...'
+                })
+            }
+        })
+    }
 }
 
 
