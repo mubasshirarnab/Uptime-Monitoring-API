9f933402d581d9c38fa4f8c382c75f3dfb997810
unknown <arnab0574@gmail.com>
2026-02-20 15:29:11 +0600

    Handle the route for Token

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 //Dependencies
 const {sampleHandler} = require('./handlers/routeHandlers/sampleHandler')
 const {userHandler} = require('./handlers/routeHandlers/userHandler')
+const {tokenHandler} = require('./handlers/routeHandlers/tokenHandler')
+
 
 
 //Tracking the Routes
 const routes = {
     sample : sampleHandler,
     user : userHandler,
+    token : tokenHandler
 }
 
 module.exports = routes
